fecbbf3a6780d32de7b4324dcbc2e2bd172c3a76
stas <chousetheone@gmail.com>
2024-10-15 13:15:17 +0300

    2

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('/etc/ssl/certs/selfsigned.crt', '/etc/ssl/private/selfsigned.key'))
+
