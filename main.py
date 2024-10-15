d75ba4cf651180c71880664b2fdfeebe6b33cad2
stas <chousetheone@gmail.com>
2024-10-15 15:23:29 +0300

    6

--- main.py
+++ main.py
@@ -3,45 +3,35 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = 'my_verify_token'  # Ваш токен для верификации
-ACCESS_TOKEN = 'EAAWbQZCzFS0YBOxXeltZCmb5nkp74RXOeqGJTWZAPEqSW2lkkbZADV5W6ZAZBsu8Xbthsx0BcPNpyiXZBlgPveYZBvp8HshPzFrnANEjTy3ZCmTsQvaE4alClev7X7vgTcZAmJyZAHLJFTHhAvAQPC5znFSwvsIpRalyC30FQisXSDQZBpVFmMa15yZCGj1hgYxgUFZAz0zXoMlz3xsJnZBIc6ZCdNVcZC6qjKZA8ZD'  # Замените на маркер доступа, сгенерированный для вашей страницы
+VERIFY_TOKEN = 'my_verify_token'
+ACCESS_TOKEN = 'EAAWbQZCzFS0YBOxXeltZCmb5nkp74RXOeqGJTWZAPEqSW2lkkbZADV5W6ZAZBsu8Xbthsx0BcPNpyiXZBlgPveYZBvp8HshPzFrnANEjTy3ZCmTsQvaE4alClev7X7vgTcZAmJyZAHLJFTHhAvAQPC5znFSwvsIpRalyC30FQisXSDQZBpVFmMa15yZCGj1hgYxgUFZAz0zXoMlz3xsJnZBIc6ZCdNVcZC6qjKZA8ZD'
 
-# Данные приложения Instagram
-INSTAGRAM_APP_ID = '27006061152371078'  # ID приложения
-INSTAGRAM_APP_SECRET = '0b0fe9aea801f3c4cae3e54615826af9'  # Секрет приложения (Не оставляйте его открытым в реальных приложениях)
+INSTAGRAM_APP_ID = '27006061152371078'
 INSTAGRAM_API_VERSION = 'v21.0'
 
-
-# Эндпоинт для верификации Webhook от Instagram
 @app.route('/webhook', methods=['GET'])
 def verify():
     if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == VERIFY_TOKEN:
         return request.args['hub.challenge'], 200
     return 'Verification failed', 403
 
-
-# Эндпоинт для обработки уведомлений от Instagram
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
     print("Получено сообщение:", data)
 
-    # Если получено сообщение
     if 'messaging' in data['entry'][0]:
-        sender_id = data['entry'][0]['messaging'][0]['sender']['id']  # ID отправителя
-        message_text = data['entry'][0]['messaging'][0]['message']['text']  # Текст сообщения
+        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+        message_text = data['entry'][0]['messaging'][0]['message']['text']
 
-        # Формируем ответное сообщение
         response_text = f"Вы отправили: {message_text}"
 
-        # Отправляем ответ через Instagram API
         send_message(sender_id, response_text)
 
     return "OK", 200
 
-
 def send_message(sender_id, message_text):
-    url = f"https://graph.instagram.com/{INSTAGRAM_API_VERSION}/me/messages"
+    url = f"https://graph.facebook.com/{INSTAGRAM_API_VERSION}/{INSTAGRAM_APP_ID}/messages"
     headers = {
         'Authorization': f'Bearer {ACCESS_TOKEN}',
         'Content-Type': 'application/json'
@@ -58,7 +48,6 @@ def send_message(sender_id, message_text):
     response = requests.post(url, json=payload, headers=headers, verify=False)
     print(f"Ответ отправлен: {response.status_code}, {response.text}")
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, ssl_context=(
-    '/etc/letsencrypt/live/stanis4live.su/fullchain.pem', '/etc/letsencrypt/live/stanis4live.su/privkey.pem'))
+        '/etc/letsencrypt/live/stanis4live.su/fullchain.pem', '/etc/letsencrypt/live/stanis4live.su/privkey.pem'))
